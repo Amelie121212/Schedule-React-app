5c6fc75de55e7fa77a7aa138ab66cc3eee70d45f
Amelie121212 <amel.1989ab@gmail.com>
2020-05-06 01:46:31 +0100

    Fifth commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import './App.css';
-import { Inject,ScheduleComponent,Day,Week,WorkWeek,Month,Agenda } from "@syncfusion/ej2-react-schedule";
+import { Inject,ScheduleComponent,Day,Week,WorkWeek,Month, ViewDirective } from "@syncfusion/ej2-react-schedule";
 
 
-class App extends React.Component{
+
+export default class App extends React.Component<{}, {}> {
+  private localData = [
+    {
+      Id:1,
+      Subject: "Meditation Time",
+      StartTime: new Date(2019,4,7,6,0),
+      EndTime: new Date(2019,4,7,6,30),
+      Location: "Yoga Center"
+    },
+    {
+      Id: 2,
+      Subject: "Skating Class",
+      StartTime: new Date(2020,5,6,7,30),
+      EndTime: new Date(2020,5,6,8,0),
+      Location: "Tower park"
+    }
+  ];
+  private eventTemplate(props:{[key:string]:Object}): JSX.Element {
+    return(<div className="month-template-wrap">{props.Location}</div>);
+  }
+  private weekEventTemplate(props:{[key:string]:Object}): JSX.Element {
+    return(<div className="template-wrap">{props.Subject}</div>);
+  }
   public render () {
     return (
-      <ScheduleComponent currentView="Month">
-        <Inject services={[Day,Week,WorkWeek,Month,Agenda]}/>
+      <ScheduleComponent width="100%" height="550px" selectedDate={new Date(2020,5,6)}  eventSettings={{dataSource: this.localData }} >
+        <ViewDirective>
+          <ViewDirective option="Day"/>
+          <ViewDirective option="Week" eventTemplate={this.weekEventTemplate.bind(this)}/>
+          <ViewDirective option="WorkWeek"/>
+          <ViewDirective option="Month" eventTemplate={this.eventTemplate.bind(this)}/>
+        </ViewDirective>
+        <Inject services={[Day,Week,WorkWeek,Month]}/>
       </ScheduleComponent>
     );
-
-  } 
+  }
 }
-
-export default App;
